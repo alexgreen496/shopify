8da92cbb22ab020a6d3eef2f94e587fa20f3767c
Alex Green <95924027+grnale011@users.noreply.github.com>
2022-03-03 19:04:04 +0200

    fixed dim_apps

--- models/dim_apps.sql
+++ models/dim_apps.sql
@@ -1,28 +1,14 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='view') }}
 
-with reviews as (
+with apps as (
 
     select *
 
-    from {{ ref('stg_reviews') }}
+    from {{ ref('stg_apps') }}
 
 
 )
 
 select *
-from reviews
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+from apps
